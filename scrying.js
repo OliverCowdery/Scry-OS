fc1b5b0fb988281f2b507f46512229d0399814c4
OliverCowdery <OliverCowdery@users.noreply.github.com>
2015-08-26 10:53:46 -0400

    Update scrying.js

--- scrying.js
+++ scrying.js
@@ -119,7 +119,7 @@ result = function () {
         shuffle(revelations);
     }
     $("#revelation").html(revelations[revIdx]);
-    $("#revelation").fadeIn("slow").delay(2000).fadeOut("slow");
+    $("#revelation").fadeIn("slow").delay(2200).fadeOut("slow");
     revIdx++;
 }
 
